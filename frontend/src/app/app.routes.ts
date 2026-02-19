f4d4d1523b6d46c616b8e6c305ce05cd794aec7f
Hayssa <hayssa.souza@sou.ufac.br>
2026-02-19 18:54:41 -0500

    feat: pagina de cadastro de doador

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 // isso aqui definie a home como pagina inicial galera
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
 ];
