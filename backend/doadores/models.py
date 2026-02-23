25d283ce42daee02768d10ddf5707d07a9f53353
Kelvin-Moreira <kelvinmreira2022@gmail.com>
2026-02-23 17:18:29 -0500

    feat: logica de deletar imagem se o doador for deletado

--- backend/doadores/models.py
+++ backend/doadores/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.dispatch import receiver
 from usuarios.models import Usuario 
 from choices import Sexo, TipoSanguineo, FatorRH
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import os
 
 class Doador(Usuario):
     sexo = models.CharField(max_length=1, choices=Sexo.choices, null=True, blank=True) 
@@ -10,3 +14,11 @@ class Doador(Usuario):
     
     def __str__(self):
         return f"Doador: {self.nome_completo}"
+
+# após um Doador ser deletado, remove a imagem na pasta de media
+@receiver(post_delete, sender=Doador)
+def deletar_arquivo_carteira(sender, instance, **kwargs):
+    if instance.carteira_doador:
+        caminho_arquivo = instance.carteira_doador.path
+        if os.path.isfile(caminho_arquivo):
+             os.remove(caminho_arquivo)
