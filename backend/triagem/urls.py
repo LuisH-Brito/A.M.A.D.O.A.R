252121d34d1aa01b75602528c6487780bfcf4bc9
LuisH-Brito <luishsbb1570@gmail.com>
2026-02-28 22:00:03 -0500

    feat: listar questionarios por cpf

--- backend/triagem/urls.py
+++ backend/triagem/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PerguntaViewSet, SalvarQuestionarioView
+from .views import ListarQuestionariosView, PerguntaViewSet, SalvarQuestionarioView
 
 router = DefaultRouter()
 router.register(r'perguntas', PerguntaViewSet, basename='pergunta')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('questionarios/', SalvarQuestionarioView.as_view(), name='salvar-questionario'),
+    path('listar-questionarios/', ListarQuestionariosView.as_view(), name='listar-questionarios'),
+    path('', include(router.urls)),
 ]
--- backend/triagem/views.py
+++ backend/triagem/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Pergunta, Questionario, Resposta
 from .serializers import PerguntaSerializer
+from doadores.models import Doador
 
 class PerguntaViewSet(viewsets.ReadOnlyModelViewSet):
     #get 
@@ -14,8 +15,7 @@ class SalvarQuestionarioView(APIView):
         dados = request.data 
 
         #como ainda nao tem um login funcional usaremos o primeiro usuario do banco para teste
-        from doadores.models import Doador 
-        doador_teste = Doador.objects.first()
+        doador_teste = Doador.objects.filter(cpf="teste").first()
         if not doador_teste:
             return Response({"erro": "Crie pelo menos um doador no Admin para testar!"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -34,3 +34,34 @@ class SalvarQuestionarioView(APIView):
                 )
 
         return Response({"mensagem": "Questionário salvo com sucesso!"}, status=status.HTTP_201_CREATED)
+    
+class ListarQuestionariosView(APIView):
+    def get(self, request):
+        cpf_buscado = request.query_params.get('cpf')
+        
+        if not cpf_buscado:
+            return Response({"erro": "CPF não informado na URL"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        questionarios = Questionario.objects.filter(doador__cpf=cpf_buscado).order_by('-data_hora_submissao')
+        
+        resultado_final = []
+        
+        for q in questionarios:
+            respostas_db = Resposta.objects.filter(questionario=q)
+            lista_respostas = []
+            
+            for r in respostas_db:
+                lista_respostas.append({
+                    "pergunta_texto": r.pergunta.texto,
+                    "resposta_dada": r.resposta_texto,
+                    "resposta_esperada": r.pergunta.resposta_esperada,
+                    "motivo_inaptidao": r.pergunta.motivo_inaptidao
+                })
+                
+            resultado_final.append({
+                "id": q.id,
+                "data_hora_submissao": q.data_hora_submissao,
+                "respostas": lista_respostas
+            })
+            
+        return Response(resultado_final, status=status.HTTP_200_OK)
