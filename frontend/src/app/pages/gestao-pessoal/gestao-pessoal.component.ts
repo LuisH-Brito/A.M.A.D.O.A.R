83bedfc62720bd867dcb8fb3edc51b496c5e2149
Hayssa <hayssa.souza@sou.ufac.br>
2026-03-01 18:14:28 -0500

    feat: adiciona mensagem de confirmação de cadastro

--- frontend/src/app/pages/gestao-pessoal/gestao-pessoal.component.ts
+++ frontend/src/app/pages/gestao-pessoal/gestao-pessoal.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gestao-pessoal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './gestao-pessoal.component.html',
-  styleUrl: './gestao-pessoal.component.scss'
+  styleUrl: './gestao-pessoal.component.scss',
 })
 export class GestaoPessoalComponent {
+  mensagemSucesso: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      if (params['cadastrado'] === 'true') {
+        this.mensagemSucesso = 'Funcionário cadastrado com sucesso!';
+
+        setTimeout(() => {
+          this.mensagemSucesso = null;
+        }, 4000);
+      }
+    });
+  }
 }
