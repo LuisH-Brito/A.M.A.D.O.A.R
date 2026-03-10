854e2d09eed76c2661903eddb2a28353c9defb8f
saames <samuel.calderadias@gmail.com>
2026-03-10 16:34:03 -0500

    feat: Garante que o botão 'Validação de Bolsas' seja exibido apenas para usuários do tipo 'Médico'

--- frontend/src/app/pages/processo-doacao-med/processo-doacao-med.component.ts
+++ frontend/src/app/pages/processo-doacao-med/processo-doacao-med.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-processo-doacao-med',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './processo-doacao-med.component.html',
   styleUrl: './processo-doacao-med.component.scss'
 })
 export class ProcessoDoacaoMedComponent {
+  cargoUsuario = localStorage.getItem('cargo') || '';
 
+  get podeValidacao(): boolean {
+    return this.cargoUsuario === 'medico';
+  }
 }
